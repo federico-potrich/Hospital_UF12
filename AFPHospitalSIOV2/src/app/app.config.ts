537d1d7b838bae85c256483f987150ca5ae96f2d
federico-potrich <federico.potrich@mat.tn.it>
2025-05-17 18:40:56 +0200

    insatllato primeNG

--- AFPHospitalSIOV2/src/app/app.config.ts
+++ AFPHospitalSIOV2/src/app/app.config.ts
@@ -1,17 +1,27 @@
-import {ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import {AFPHospitalAPIService} from './core/services/afphospital-api.service';
+import { AFPHospitalAPIService } from './core/services/afphospital-api.service';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAppInitializer(() =>
-      inject(AFPHospitalAPIService).getListaPazienti()
-    ),
-  ]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideHttpClient(),
+        provideAppInitializer(() =>
+            inject(AFPHospitalAPIService).getListaPazienti()
+        ),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+    ]
 };
