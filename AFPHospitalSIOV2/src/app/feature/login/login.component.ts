8eb0bbcfdf73574d13e391feda6fc08b6d91acba
federico-potrich <federico.potrich@mat.tn.it>
2025-05-19 12:18:00 +0200

    modifiche

--- AFPHospitalSIOV2/src/app/feature/login/login.component.ts
+++ AFPHospitalSIOV2/src/app/feature/login/login.component.ts
@@ -1,5 +1,5 @@
 import { gsap } from 'gsap';
-import { AfterViewInit, Component, inject } from '@angular/core';
+import { AfterViewInit, Component, computed, inject, signal } from '@angular/core';
 
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
@@ -10,32 +10,41 @@ import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
 gsap.registerPlugin(DrawSVGPlugin);
 
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+
 import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../core/services/login/login.service';
 import { Router } from '@angular/router';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-login',
-  imports: [IconFieldModule, InputIconModule, FormsModule, PasswordModule, ButtonModule, InputGroupModule, InputGroupAddonModule],
+  imports: [IconFieldModule, InputIconModule, FormsModule, PasswordModule, ButtonModule, InputGroupModule, InputGroupAddonModule, Toast, MessageModule ],
+  providers: [MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent implements AfterViewInit{
-
+export class LoginComponent implements AfterViewInit {
+  #messageService = inject(MessageService)
   #service = inject(LoginService)
+  visible = signal<boolean>(false);
 
   credentials: any = {
     username: "",
     password: ""
   }
-  constructor() {
-    if(this.#service.logged()){
+  constructor(private messageService: MessageService) {
+    if (this.#service.logged()) {
       inject(Router).navigate([''])
     }
   }
 
-  login(){
-    this.#service.login(this.credentials.username, this.credentials.password)
+  login() {
+    if(this.#service.login(this.credentials.username, this.credentials.password)==false){
+      this.messageService.add({ severity: 'error', summary: 'Login Failed', detail: 'Your email or password is incorrect.\n\rPlease try again.' , sticky: true });
+      this.visible.set(true);
+    }
   }
 
   ngAfterViewInit(): void {
