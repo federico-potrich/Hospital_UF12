3c249aadf91af623e4ea0af5c3e53d1a093087f1
federico-potrich <federico.potrich@mat.tn.it>
2025-05-31 10:35:56 +0200

    aggiunta rotte degli ospedali

---
+++ AFPHospitalAPI/ospedali.ts
@@ -0,0 +1,37 @@
+
+import * as mysql from 'mysql2/promise';
+import { createHttpResponceKO, createHttpResponceOK } from './ResponceManager';
+
+// import { process } from 'node:process'
+
+// npm i -D @types/node
+const dbConfig = {
+    host: "localhost",
+    user: "root",       // Cambia se necessario
+    password: "root",       // Cambia se necessario
+    database: "ospedale",
+    port: 3306,
+};
+
+export const listaOspedali = async () => {
+    let connection;
+
+    try {
+        // Connesione al DB
+        connection = await mysql.createConnection(dbConfig);
+
+        // Eseguiamo la query
+        const [row] = await connection.execute(`
+            SELECT 
+                o.id as id_reparto,
+                o.nome
+            FROM Ospedali o
+        `);
+
+        return createHttpResponceOK(row);
+    } catch (error) {
+        return createHttpResponceKO(error);
+    } finally{
+        connection.end();
+    }
+};
